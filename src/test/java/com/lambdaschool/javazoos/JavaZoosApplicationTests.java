6d5220b8bcc3cfe811979959940eb490adec2707
sagardesai90 <saagardesai@gmail.com>
2019-02-27 08:46:30 -0800

    first commit

---
+++ src/test/java/com/lambdaschool/javazoos/JavaZoosApplicationTests.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.javazoos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaZoosApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
